ecf9812e16cfea4d137db51eb1aeb084026bd10d
lostmart <lostmart@hotmail.com>
2024-11-21 15:07:27 +0100

    new head tags

--- src/main.tsx
+++ src/main.tsx
@@ -10,25 +10,31 @@ import Home from "./pages/Home.tsx"
 import ErrorPage from "./pages/ErrorPage.tsx"
 import BannerComp from "./components/BannerComp.tsx"
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <Home />,
+			errorElement: <ErrorPage />,
+		},
+	],
 	{
-		path: "/",
-		element: <Home />,
-		errorElement: <ErrorPage />,
-	},
-])
+		future: {
+			v7_relativeSplatPath: true,
+			v7_fetcherPersist: true,
+			v7_normalizeFormMethod: true,
+			v7_partialHydration: true,
+			v7_skipActionErrorRevalidation: true,
+		},
+	}
+)
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<>
 		<BannerComp text="Task Management" />
 		<main className="bg-zinc-100">
 			<React.StrictMode>
-				<RouterProvider
-					router={router}
-					future={{
-						v7_startTransition: true,
-					}}
-				/>
+				<RouterProvider router={router} future={{ v7_startTransition: true }} />
 			</React.StrictMode>
 		</main>
 	</>
