fc542e8014a4b496b606aa9160b1fb2fcb4bd038
lostmart <lostmart@hotmail.com>
2024-11-25 17:01:57 +0100

    new styuff

--- src/components/layout/UserComp.tsx
+++ src/components/layout/UserComp.tsx
@@ -1,9 +1,10 @@
 import InputGroup from "../ui/InputGroup"
+import { FaRegEnvelope } from "react-icons/fa6"
 
 const UserComp = () => {
 	return (
 		<form>
-			<InputGroup inputId="email" />
+			<InputGroup inputId="email" icon={FaRegEnvelope} />
 		</form>
 	)
 }
--- src/components/ui/InputGroup.tsx
+++ src/components/ui/InputGroup.tsx
@@ -1,11 +1,16 @@
+import { IconType } from "react-icons"
+
 interface InputGroupProps {
 	inputId: string
+	icon: IconType
 }
 
-const InputGroup = ({ inputId }: InputGroupProps) => {
+const InputGroup = ({ inputId, icon: Icon }: InputGroupProps) => {
 	return (
-		<div className="flex">
-			<label htmlFor={inputId}>icon here</label>
+		<div className="flex border-solid border-zinc-400 border-2">
+			<label htmlFor={inputId} className="flex justify-center p-3">
+				<Icon />
+			</label>
 			<input type="text" id={inputId} placeholder={inputId} className="p-4" />
 		</div>
 	)
