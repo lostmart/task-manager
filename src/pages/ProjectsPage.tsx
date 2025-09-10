3a159be5eee818c4ab93d184249eb20808a7f64f
lostmart <lostmart@hotmail.com>
2025-09-10 22:12:58 +0200

    projects page, hardcoded data

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,10 +1,39 @@
-import React from "react"
+import { FaEllipsisVertical } from "react-icons/fa6"
 
 const ProjectsPage = () => {
 	return (
-		<div>
-			<h1>ProjectsPage</h1>
-		</div>
+		<section className="bg-white">
+			<h1 className="text-4xl text-center">ProjectsPage</h1>
+			<div className="container-xl lg:container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+				<article className="flex flex-col items-center p-4 bg-slate-100 mt-2 relative">
+					<h2 className="flex justify-between w-full">
+						Title 1
+						<button className="p-2 bg-slate-100">
+							<FaEllipsisVertical />
+						</button>
+					</h2>
+					<p>Project description</p>
+				</article>
+				<article className="flex flex-col items-center p-4 bg-slate-100 mt-2 relative">
+					<h2 className="flex justify-between w-full">
+						Title 1
+						<button className="p-2 bg-slate-100">
+							<FaEllipsisVertical />
+						</button>
+					</h2>
+					<p>Project description</p>
+				</article>
+				<article className="flex flex-col items-center p-4 bg-slate-100 mt-2 relative">
+					<h2 className="flex justify-between w-full">
+						Title 1
+						<button className="p-2 bg-slate-100">
+							<FaEllipsisVertical />
+						</button>
+					</h2>
+					<p>Project description</p>
+				</article>
+			</div>
+		</section>
 	)
 }
 
