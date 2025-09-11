33a37cf669ed8d8a5e6afc2ea6ac15b6f96097d5
lostmart <martinpedraza1979@gmail.com>
2025-09-11 16:48:47 +0200

    single pages

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -6,15 +6,6 @@ const ProjectsPage = () => {
 		<section className="bg-white">
 			<h1 className="text-4xl text-center py-3">ProjectsPage</h1>
 			<div className="container-xl p-2 lg:container mx-auto grid grid-cols-1 md:grid-cols-2 md:p-3 lg:grid-cols-3 gap-4">
-				<article className="flex flex-col items-center p-4 bg-slate-100 mt-2 relative">
-					<h2 className="flex justify-between w-full">
-						Title 1
-						<button className="p-2 bg-slate-100">
-							<FaEllipsisVertical />
-						</button>
-					</h2>
-					<p>Project description</p>
-				</article>
 				<Link to="/projects/1">
 					<article className="flex flex-col items-center p-4 bg-slate-100 mt-2 relative">
 						<h2 className="flex justify-between w-full">
@@ -26,15 +17,28 @@ const ProjectsPage = () => {
 						<p>Project description</p>
 					</article>
 				</Link>
-				<article className="flex flex-col items-center p-4 bg-slate-100 mt-2 relative">
-					<h2 className="flex justify-between w-full">
-						Title 1
-						<button className="p-2 bg-slate-100">
-							<FaEllipsisVertical />
-						</button>
-					</h2>
-					<p>Project description</p>
-				</article>
+				<Link to="/projects/1">
+					<article className="flex flex-col items-center p-4 bg-slate-100 mt-2 relative">
+						<h2 className="flex justify-between w-full">
+							Title 1
+							<button className="p-2 bg-slate-100">
+								<FaEllipsisVertical />
+							</button>
+						</h2>
+						<p>Project description</p>
+					</article>
+				</Link>
+				<Link to="/projects/1">
+					<article className="flex flex-col items-center p-4 bg-slate-100 mt-2 relative">
+						<h2 className="flex justify-between w-full">
+							Title 1
+							<button className="p-2 bg-slate-100">
+								<FaEllipsisVertical />
+							</button>
+						</h2>
+						<p>Project description</p>
+					</article>
+				</Link>
 			</div>
 		</section>
 	)
--- src/pages/SingleProject.tsx
+++ src/pages/SingleProject.tsx
@@ -1,5 +1,12 @@
 const SingleProject = () => {
-	return <div>SingleProject</div>
+	return <section>
+		<h1 className="text-3xl text-center py-3">SingleProject</h1>
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+		</ul>
+	</section>
 }
 
 export default SingleProject
